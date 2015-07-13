92eda3651b18944ce7e3f0bccd75149c2ca6280a
Kirill Gorbushko <kirill.ge@gmail.com>
2015-07-13 23:12:34 +0300

    initial commit

---
+++ TRA Smart Services/SupportingClasses/AppDelegate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TRA Smart Services
+//
+//  Created by Kirill Gorbushko on 13.07.15.
+//  Copyright © 2015 Thinkmobiles. All rights reserved.
+//
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TRA Smart Services/SupportingClasses/AppHelper/AppHelper.m
@@ -0,0 +1,13 @@
+//
+//  AppHelper.m
+//  TRA Smart Services
+//
+//  Created by Kirill Gorbushko on 13.07.15.
+//  Copyright © 2015 Thinkmobiles. All rights reserved.
+//
+
+#import "AppHelper.h"
+
+@implementation AppHelper
+
+@end
