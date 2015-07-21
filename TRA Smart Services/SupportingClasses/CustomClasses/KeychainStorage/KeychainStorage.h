01aed4ff349da22044d701626e397827b078c51d
Kirill Gorbushko <kirill.ge@gmail.com>
2015-07-21 14:49:58 +0300

    added keychain logic

---
+++ TRA Smart Services/SupportingClasses/CustomClasses/KeychainStorage/KeychainStorage.h
@@ -0,0 +1,18 @@
+//
+//  KeychainStorage.h
+//  TRA Smart Services
+//
+//  Created by Kirill Gorbushko on 21.07.15.
+//  Copyright © 2015 Thinkmobiles. All rights reserved.
+//
+
+static NSString *const KeychainStorageKeyLogin = @"KeychainStorageKeyLogin";
+static NSString *const KeychainStorageKeyPassword = @"KeychainStorageKeyPassword";
+
+@interface KeychainStorage : NSObject
+
+- (void)storePassword:(NSString *)password forUser:(NSString *)userName;
+- (void)removeStoredCredentials;
+- (NSDictionary *)credentialsForLoginedUser;
+
+@end
