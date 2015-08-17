de5aff17a3529ed1302097c35bcd401c199804a5
kirillgorbushko <kirill@Kirills-MacBook-Pro.local>
2015-08-17 09:17:42 +0300

    added base searchableViewController

---
+++ TRA Smart Services/Classes/ViewControllers/FavouriteViewController/FavouriteViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavouriteViewController.h
+//  TRA Smart Services
+//
+//  Created by Kirill Gorbushko on 14.08.15.
+//  Copyright (c) 2015 Thinkmobiles. All rights reserved.
+//
+
+#import "BaseSearchableViewController.h"
+
+@interface FavouriteViewController : BaseSearchableViewController 
+
+@end
---
+++ TRA Smart Services/Classes/ViewControllers/FavouriteViewController/FavouriteViewController.m
@@ -0,0 +1,44 @@
+//
+//  FavouriteViewController.m
+//  TRA Smart Services
+//
+//  Created by Kirill Gorbushko on 14.08.15.
+//  Copyright (c) 2015 Thinkmobiles. All rights reserved.
+//
+
+#import "FavouriteViewController.h"
+#import "Animation.h"
+
+@interface FavouriteViewController ()
+
+@end
+
+@implementation FavouriteViewController
+
+#pragma mark - LifeCycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    NSLog(@"text - %@", searchText);
+}
+
+#pragma mark - Superclass Methods
+
+- (void)localizeUI
+{
+    self.searchanbeleViewControllerTitle.text = dynamicLocalizedString(@"favourite.title");
+}
+
+- (void)updateColors
+{
+    
+}
+
+@end
