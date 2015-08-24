9ae9e4313cdce41bc6b0b7d29c600614e9edf378
Kirill Gorbushko <kirillgorbushko@MacBook-Pro-Kirill.local>
2015-08-24 11:06:45 +0300

    undo

--- TRA Smart Services/Classes/ViewControllers/FavouriteViewController/FavouriteViewController.m
+++ TRA Smart Services/Classes/ViewControllers/FavouriteViewController/FavouriteViewController.m
@@ -407,7 +407,7 @@ static NSString *const ServiceInfoListSegueIdentifier = @"serviceInfoListSegue";
     
     [self.arcDeleteZoneLayer addSublayer:self.contentFakeIconLayer];
     [self.arcDeleteZoneLayer insertSublayer:self.shadowFakeIconLayer below:self.contentFakeIconLayer];
-    [self.view.layer addSublayer:self.arcDeleteZoneLayer];
+    [self.tableView.layer addSublayer:self.arcDeleteZoneLayer];
     
     [self animateDeleteZoneAppearence];
 }
