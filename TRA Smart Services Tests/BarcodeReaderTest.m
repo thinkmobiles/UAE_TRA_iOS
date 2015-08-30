2d1dff0204175b85f1f4b5f1e99096c40174c7e1
Kirill Gorbushko <kiril.gorbushko@thinkmobiles.com; kirill.ge@gmail.com>
2015-08-30 12:50:44 +0300

    added simple barcode test

---
+++ TRA Smart Services Tests/BarcodeReaderTest.m
@@ -0,0 +1,38 @@
+//
+//  BarcodeReaderTest.m
+//  TRA Smart Services
+//
+//  Created by Kirill Gorbushko on 30.08.15.
+//  Copyright (c) 2015 Thinkmobiles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "BarcodeCodeReader.h"
+
+@interface BarcodeReaderTest : XCTestCase
+
+@property (strong, nonatomic) UIView *fakePlayerView;
+@property (strong, nonatomic) BarcodeCodeReader *reader;
+
+@end
+
+@implementation BarcodeReaderTest
+
+- (void)testInitWithNilView
+{
+    self.reader = [[BarcodeCodeReader alloc] initWithView:self.fakePlayerView];
+    XCTAssertFalse(self.reader);
+}
+
+- (void)testInitWithView
+{
+    self.fakePlayerView = [[UIView alloc] init];
+    self.reader = [[BarcodeCodeReader alloc] initWithView:self.fakePlayerView];
+    XCTAssert(self.reader);
+}
+
+
+
+@end
