e29d5cb38e74dcde11e315db1139ed919efa71e8
Kirill Gorbushko <kiril.gorbushko@thinkmobiles.com; kirill.ge@gmail.com>
2015-09-16 00:54:21 +0300

    update home notification screen

--- TRA Smart Services/Classes/ViewControllers/NotificationsViewController/NotificationViewController.m
+++ TRA Smart Services/Classes/ViewControllers/NotificationsViewController/NotificationViewController.m
@@ -37,7 +37,8 @@
     }
     [[UIApplication sharedApplication] setStatusBarHidden:YES];
     
-    [self prepareDataSource];
+   // [self prepareDataSource]; //temp
+    [self showPlaceHolderIfNeededAnimated:NO];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -61,13 +62,7 @@
 - (void)setDataSource:(NSMutableArray *)dataSource
 {
     _dataSource = dataSource;
-    if (self.dataSource.count) {
-        self.placeHolderNotificationLabel.hidden = YES;
-        self.conteinerClearAllButtonView.hidden = NO;
-    } else {
-        self.placeHolderNotificationLabel.hidden = NO;
-        self.conteinerClearAllButtonView.hidden = YES;
-    }
+    [self showPlaceHolderIfNeededAnimated:YES];
 }
 
 #pragma mark - IBActions
@@ -89,7 +84,8 @@
     [self.dataSource removeAllObjects];
     [self.tableView endUpdates];
     
-    [self displayNotificationsData];
+    [self showActiveNotificationStatus];
+    [self showPlaceHolderIfNeededAnimated:YES];
 }
 
 #pragma mark - UITableViewDataSource
@@ -129,7 +125,8 @@
                      withRowAnimation:UITableViewRowAnimationFade];
     [self.dataSource removeObjectAtIndex:indexPath.row];
     [self.tableView endUpdates];
-    [self displayNotificationsData];
+    [self showActiveNotificationStatus];
+    [self showPlaceHolderIfNeededAnimated:YES];
 }
 
 #pragma mark - Private
@@ -155,15 +152,21 @@
     self.informableLabel.layer.transform = animCATransform3D;
 }
 
-- (void)showPlaceHolderIfNeeded
+- (void)showPlaceHolderIfNeededAnimated:(BOOL)animated
 {
     if (self.dataSource.count) {
         self.placeHolderNotificationLabel.hidden = YES;
+        self.conteinerClearAllButtonView.layer.opacity = 1.0f;
         self.conteinerClearAllButtonView.hidden = NO;
     } else {
         self.placeHolderNotificationLabel.hidden = NO;
-        [self.placeHolderNotificationLabel.layer addAnimation:[Animation fadeAnimFromValue:0.f to:1.0f delegate:nil] forKey:nil];
-        [self.conteinerClearAllButtonView.layer addAnimation:[Animation fadeAnimFromValue:1.0f to:0.0f delegate:self] forKey:@"keyAnimationPlaceHolder"];
+        if (animated) {
+            [self.placeHolderNotificationLabel.layer addAnimation:[Animation fadeAnimFromValue:0.f to:1.0f delegate:nil] forKey:nil];
+            [self.conteinerClearAllButtonView.layer addAnimation:[Animation fadeAnimFromValue:1.0f to:0.0f delegate:self] forKey:@"keyAnimationPlaceHolder"];
+            self.conteinerClearAllButtonView.layer.opacity = 0.0f;
+        } else {
+            self.conteinerClearAllButtonView.hidden = YES;
+        }
     }
 }
 
@@ -186,13 +189,12 @@
 - (void)localizeUI
 {
     self.placeHolderNotificationLabel.text = dynamicLocalizedString(@"notificationsViewController.NotificationPlaceHolderlabel");
-    [self displayNotificationsData];
     [self.clearAllButton setTitle:dynamicLocalizedString(@"notificationsViewController.clearAllButton.title") forState:UIControlStateNormal];
+    [self showActiveNotificationStatus];
 }
 
-- (void)displayNotificationsData
+- (void)showActiveNotificationStatus
 {
-    [self showPlaceHolderIfNeeded];
     if (self.dataSource.count) {
         self.informableLabel.text = [NSString stringWithFormat:dynamicLocalizedString(@"notificationsViewController.NotificationsMessage"), (int)self.dataSource.count];
     } else {
