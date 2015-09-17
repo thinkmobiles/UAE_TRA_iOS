c282fc7652c95c4f8a8887d57e5bfe446e0ef164
RomaVizenko <Roma@MacBook-Pro-Roma.local>
2015-09-17 17:52:51 +0300

    add ServiceHeaderView, ServiceSelectionView

---
+++ TRA Smart Services/Classes/Views/ServiceSelectionView/ServiceSelectionView.h
@@ -0,0 +1,35 @@
+//
+//  ServiceSelectionView.h
+//  TRA Smart Services
+//
+//  Created by RomaVizenko on 17.09.15.
+//  Copyright (c) 2015 Thinkmobiles. All rights reserved.
+//
+
+#import "BaseXibView.h"
+
+@protocol SelectedServiseViewDelegate <NSObject>
+
+@required
+- (void)buttonReportSMSDidTapped;
+- (void)buttonReportWEBDidTapped;
+- (void)buttonViewMyListDidTapped;
+
+@end
+
+@interface ServiceSelectionView : BaseXibView
+
+@property (weak, nonatomic) id <SelectedServiseViewDelegate> delegate;
+
+@property (weak, nonatomic) IBOutlet UILabel *reportSMSLabel;
+@property (weak, nonatomic) IBOutlet UILabel *reportWEBLabel;
+@property (weak, nonatomic) IBOutlet UILabel *viewMyListLabel;
+
+@property (strong, nonatomic) UIImage *serviceNameReportSMSImage;
+@property (strong, nonatomic) UIImage *serviceNameReportWEBImage;
+@property (strong, nonatomic) UIImage *serviceNameViewMyListImage;
+
+- (void)setRTLArabicUIStyle;
+- (void)setLTREuropeUIStyle;
+
+@end
